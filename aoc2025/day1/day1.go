378a5d4fcf5e7a70301a029311c413f246f4b241
Kate Shaffer <kate.shaffer@gmail.com>
2025-12-30 15:53:01 -0800

    WIP: Day 1, part 2 *almost* works, overcounting boundary zeros

--- aoc2025/day1/day1.go
+++ aoc2025/day1/day1.go
@@ -7,61 +7,76 @@ import (
 )
 
 func Main() {
-	//inputLines := util.ReadInputFileToStringSlice("day1/sample")
-	inputLines := util.ReadInputFileToStringSlice("day1/input")
-	totalZeros := part1(inputLines)
+	inputLines := util.ReadInputFileToStringSlice("day1/sample")
+	//inputLines := util.ReadInputFileToStringSlice("day1/input")
+	totalZeros := part2(inputLines)
 
 	fmt.Printf(">>>>>Total Zeros: %v\n", totalZeros)
 }
 
-func part1(instructionLines []string) int {
-	// start at 50
-	// track totalZeros
-
-	// for each line:
-	// - rotate func:
-	//   - rotate per instruction
-	//   - return current position (optional print)
-	// - if current position == 0: totalZeros++
-
-	// return totalZeros
-
+// modified part1 -> part2 in place
+func part2(instructionLines []string) int {
 	currentPosition := 50
 	totalZeros := 0
 	dialMax := 99
 
 	for _, line := range instructionLines {
-		currentPosition = rotate(dialMax, currentPosition, line)
+		fmt.Printf("Debug: PROCESSING instruction: %v\n", line)
+		var zeroPasses int
+		currentPosition, zeroPasses = rotate(dialMax, currentPosition, line)
 
-		fmt.Printf("Debug: CURRENT POSITION after instruction: %v == %v\n", line, currentPosition)
-		if currentPosition == 0 {
-			totalZeros++
-		}
+		totalZeros += zeroPasses
+
+		fmt.Printf("Debug: CURRENT POSITION after instruction: %v == %v. zeroPasses: %v\n\n", line, currentPosition, zeroPasses)
+		//if currentPosition == 0 {
+		//	totalZeros++
+		//}
 	}
 
 	return totalZeros
 }
 
-// returns position after rotation
-func rotate(dialMax int, currentPosition int, instruction string) int {
+// returns: position after rotation, how many times we rotated past 0
+func rotate(dialMax int, currentPosition int, instruction string) (int, int) {
 	rotationInt := parseInstruction(instruction)
 
 	mightBeOOB := currentPosition + rotationInt
 
-	fmt.Printf("Debug: MIGHT BE OOB: %v\n", mightBeOOB)
+	endPosition, zeroPasses := normalizeToDialBounds(mightBeOOB, dialMax, 0) // reset zeroPasses to 0 for every new instruction
+	return endPosition, zeroPasses
+}
 
-	var endPosition int
+// returns: in-bounds position after rotation, how many times we rotated past 0
+func normalizeToDialBounds(potentialEndPosition int, dialMax int, zeroPasses int) (int, int) {
+	fmt.Printf("Debug: MIGHT BE OOB: %v\n", potentialEndPosition)
 
-	// Assumption: only supports clicks <= 100
-	if mightBeOOB < 0 {
-		endPosition = dialMax + 1 + mightBeOOB
-	} else if mightBeOOB > dialMax {
-		endPosition = mightBeOOB - (dialMax + 1)
-	} else {
-		endPosition = mightBeOOB
+	// break condition
+	if potentialEndPosition == 0 {
+		zeroPasses++ // one more here
+		return potentialEndPosition, zeroPasses
+	} else if potentialEndPosition > 0 && potentialEndPosition <= dialMax {
+		return potentialEndPosition, zeroPasses
 	}
 
-	return endPosition
+	// recurse
+	var endPosition int
+	if potentialEndPosition < 0 {
+		endPosition = dialMax + 1 + potentialEndPosition
+
+		//if endPosition != 0 { // don't double-count if ends on zero
+		zeroPasses++
+		//}
+	} else if potentialEndPosition > dialMax {
+		endPosition = potentialEndPosition - (dialMax + 1)
+
+		//if endPosition != 0 { // don't double-count if ends on zero
+		zeroPasses++
+		//}
+	} else {
+		//println("THIS SHOULDN'T HAVE HAPPENED, bad normalizeToDialBounds condition")
+		panic("THIS SHOULDN'T HAVE HAPPENED, bad normalizeToDialBounds condition")
+	}
+	return normalizeToDialBounds(endPosition, dialMax, zeroPasses)
 }
 
 func parseInstruction(instructionString string) int {
@@ -74,7 +89,8 @@ func parseInstruction(instructionString string) int {
 	} else if direction == 'R' {
 		rotationInt, _ = strconv.Atoi(clicks)
 	} else {
-		println("OOOOPS that's not L or R")
+		//println("OOOOPS that's not L or R")
+		panic("OOOOPS that's not L or R")
 	}
 
 	return rotationInt
