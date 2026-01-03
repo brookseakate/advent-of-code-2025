b8cb73d4c575b9f19556fb731a08c648da47ec9b
Kate Shaffer <kate.shaffer@gmail.com>
2026-01-03 05:52:54 -0800

    Day 4, part 2 works

--- aoc2025/day4/day4.go
+++ aoc2025/day4/day4.go
@@ -3,19 +3,43 @@ package day4
 import (
 	"aoc2025/util"
 	"fmt"
+	"slices"
+	"strings"
 )
 
 func Main() {
 	//input := util.ReadInputFileToStringSlice("day4/sample")
 	input := util.ReadInputFileToStringSlice("day4/input")
 
-	accessiblePaperRollTotal := calculateAccessibleRollTotal(input)
-	fmt.Printf("TOTAL: %v\n", accessiblePaperRollTotal)
+	//total := calculateAccessibleRollTotal(input) // part 1
+	total := calculateRemovableRollTotal(input)
+	fmt.Printf("TOTAL: %v\n", total)
 }
 
-// part 1
-func calculateAccessibleRollTotal(input []string) int {
+// part 2
+func calculateRemovableRollTotal(input []string) int {
 	total := 0
+	justRemoved := 1
+	var updatedGrid []string
+
+	for justRemoved != 0 {
+		justRemoved, updatedGrid = removeRolls(input)
+
+		total += justRemoved
+
+		fmt.Printf("Debug: justRemoved: %v\n", justRemoved)
+		fmt.Printf("Debug: input:\n %v\n\n", updatedGrid)
+
+		input = updatedGrid
+	}
+
+	return total
+}
+
+// for part 2. Returns: totalRemoved this round, updatedGrid
+func removeRolls(input []string) (int, []string) {
+	roundTotal := 0
+	updatedGrid := slices.Clone(input)
 
 	for i, row := range input {
 		for j, _ := range row {
@@ -23,12 +47,17 @@ func calculateAccessibleRollTotal(input []string) int {
 
 			if input[i][j] == '@' && isAccessibleRoll(input, i, j) {
 				//fmt.Printf("Debug: it's accessible!\n\n")
-				total++
+				roundTotal++
+
+				splitRow := strings.Split(updatedGrid[i], "")
+				splitRow[j] = "."
+				updatedRow := strings.Join(splitRow, "")
+				updatedGrid[i] = updatedRow
 			}
 		}
 	}
 
-	return total
+	return roundTotal, updatedGrid
 }
 
 func isAccessibleRoll(input []string, i int, j int) bool {
@@ -71,4 +100,22 @@ func isAccessibleRoll(input []string, i int, j int) bool {
 	}
 
 	return false
+}
+
+// part 1
+func calculateAccessibleRollTotal(input []string) int {
+	total := 0
+
+	for i, row := range input {
+		for j, _ := range row {
+			//fmt.Printf("Debug: Examining %v, %v: %c\n", i, j, input[i][j])
+
+			if input[i][j] == '@' && isAccessibleRoll(input, i, j) {
+				//fmt.Printf("Debug: it's accessible!\n\n")
+				total++
+			}
+		}
+	}
+
+	return total
 }
