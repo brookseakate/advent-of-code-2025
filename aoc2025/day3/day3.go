d639bdf52571c281fce31ad4403832e1ae6f9699
Kate Shaffer <kate.shaffer@gmail.com>
2026-01-03 04:22:32 -0800

    Day 3, part 2 cleaned up

--- aoc2025/day3/day3.go
+++ aoc2025/day3/day3.go
@@ -44,42 +44,6 @@ func getMaxJoltageTwoBatteries(bankString string) int {
 	return currentMax
 }
 
-// part 2 – expecting this to not work due to time complexity, but will try naive version first
-func naiveGetMaxJoltageTwelveBatteries(bankString string) int {
-	currentMax := 0
-	for i, digitOne := range bankString[:len(bankString)-11] {
-		for j, digitTwo := range bankString[i+1 : len(bankString)-10] {
-			for k, digitThree := range bankString[j+1 : len(bankString)-9] {
-				for l, digitFour := range bankString[k+1 : len(bankString)-8] {
-					for m, digitFive := range bankString[l+1 : len(bankString)-7] {
-						for n, digitSix := range bankString[m+1 : len(bankString)-6] {
-							for o, digitSeven := range bankString[n+1 : len(bankString)-5] {
-								for p, digitEight := range bankString[o+1 : len(bankString)-4] {
-									for q, digitNine := range bankString[p+1 : len(bankString)-3] {
-										for r, digitTen := range bankString[q+1 : len(bankString)-2] {
-											for s, digitEleven := range bankString[r+1 : len(bankString)-1] {
-												for _, digitTwelve := range bankString[s+1:] {
-
-													joltage, _ := strconv.Atoi(fmt.Sprintf("%c%c%c%c%c%c%c%c%c%c%c%c", digitOne, digitTwo, digitThree, digitFour, digitFive, digitSix, digitSeven, digitEight, digitNine, digitTen, digitEleven, digitTwelve))
-
-													if joltage > currentMax {
-														currentMax = joltage
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-	return currentMax
-}
-
 // part 2 optimized
 func getMaxJoltageTwelveBatteries(bankString string) int {
 	digitOneInd := getRangeMaxIndex(bankString[:len(bankString)-11])
