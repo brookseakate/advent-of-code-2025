d6441c1ef536fa244f7a16a953d96a278cf16aa6
Kate Shaffer <kate.shaffer@gmail.com>
2026-01-03 03:14:34 -0800

    WIP Day 3, part 2 – naive solution times out

--- aoc2025/day3/day3.go
+++ aoc2025/day3/day3.go
@@ -23,12 +23,13 @@ func getMaxJoltages(bankStrings []string) []int {
 	var maxJoltages []int
 
 	for _, bankString := range bankStrings {
-		maxJoltages = append(maxJoltages, getMaxJoltage(bankString))
+		//maxJoltages = append(maxJoltages, getMaxJoltageTwoBatteries(bankString)) // part 1
+		maxJoltages = append(maxJoltages, naiveGetMaxJoltageTwelveBatteries(bankString)) // part 2
 	}
 	return maxJoltages
 }
 
-func getMaxJoltage(bankString string) int {
+func getMaxJoltageTwoBatteries(bankString string) int {
 	currentMax := 0
 	for i, digitOne := range bankString[:len(bankString)-1] {
 		for _, digitTwo := range bankString[i+1:] {
@@ -36,6 +37,42 @@ func getMaxJoltage(bankString string) int {
 
 			if joltage > currentMax {
 				currentMax = joltage
+			}
+		}
+	}
+	return currentMax
+}
+
+// expecting this to not work due to time complexity, but will try naive version first
+func naiveGetMaxJoltageTwelveBatteries(bankString string) int {
+	currentMax := 0
+	for i, digitOne := range bankString[:len(bankString)-12] {
+		for j, digitTwo := range bankString[i+1 : len(bankString)-11] {
+			for k, digitThree := range bankString[j+1 : len(bankString)-10] {
+				for l, digitFour := range bankString[k+1 : len(bankString)-9] {
+					for m, digitFive := range bankString[l+1 : len(bankString)-8] {
+						for n, digitSix := range bankString[m+1 : len(bankString)-7] {
+							for o, digitSeven := range bankString[n+1 : len(bankString)-6] {
+								for p, digitEight := range bankString[o+1 : len(bankString)-5] {
+									for q, digitNine := range bankString[p+1 : len(bankString)-4] {
+										for r, digitTen := range bankString[q+1 : len(bankString)-3] {
+											for s, digitEleven := range bankString[r+1 : len(bankString)-2] {
+												for _, digitTwelve := range bankString[s+1:] {
+
+													joltage, _ := strconv.Atoi(fmt.Sprintf("%c%c%c%c%c%c%c%c%c%c%c%c", digitOne, digitTwo, digitThree, digitFour, digitFive, digitSix, digitSeven, digitEight, digitNine, digitTen, digitEleven, digitTwelve))
+
+													if joltage > currentMax {
+														currentMax = joltage
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
